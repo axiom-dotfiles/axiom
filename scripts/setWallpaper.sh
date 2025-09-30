85c5d2a0bb40a6acdb748087c1ce3b4007104bfa
travmonkey <t.coding@batk.me>
2025-09-30 15:31:12 -0600

    Remove pointless comments

--- scripts/setWallpaper.sh
+++ scripts/setWallpaper.sh
@@ -15,22 +15,17 @@ fi
 
 WALLPAPER_PATH="$1"
 
-# Kill swaybg if running, as it can interfere with swww
 pkill swaybg &>/dev/null || true
 
-# Initialize swww if it's not already running
 swww query >/dev/null || swww init
 
-# Randomly select a transition type
 transitions=("wipe" "any" "outer" "wave")
 TRANSITION_TYPE=${transitions[$RANDOM % ${#transitions[@]}]}
 SWWW_PARAMS="--transition-fps 144 --transition-type $TRANSITION_TYPE --transition-duration 1"
 
-# Set the wallpaper on the currently active monitor
 current_monitor=$(hyprctl -j activeworkspace | jq -r .monitor)
 swww img -o "$current_monitor" "$WALLPAPER_PATH" $SWWW_PARAMS
 
-# Update a symlink for the primary monitor for other scripts to use
 if [[ "$current_monitor" == "DP-1" ]]; then
   ln -sf "$WALLPAPER_PATH" "$HOME/.current_wallpaper"
 fi
